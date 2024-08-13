b1b5a01b8fd7aff608927fd3c14e2695a5bdb414
Hendrik <drikdoank@gmail.com>
2024-08-13 16:08:29 +0700

    api for transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,16 +3,16 @@ class Transaction < ApplicationRecord
   belongs_to :stock
 
   validates :amount, numericality: { greater_than: 0 }
-  validates :transaction_type, inclusion: { in: %w[buy sell] }
+  validates :transaction_type, inclusion: { in: %w[credit debit] }
 
   after_create :update_wallet_balance
 
   private
 
   def update_wallet_balance
-    if transaction_type == "Credit"
+    if transaction_type == "credit"
       wallet.update(balance: wallet.balance - amount)
-    elsif transaction_type == "Debits"
+    elsif transaction_type == "debit"
       wallet.update(balance: wallet.balance + amount)
     end
   end
